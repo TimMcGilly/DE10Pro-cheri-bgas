6381e3e95f396306abc98520c3195ad8807e1770
Tim McGilly <timmcgilly@gmail.com>
2025-02-05 22:06:22 +0000

    Update run_sim to use correct paths and timeoute

--- bluespec/sim-utils/run_sim.sh
+++ bluespec/sim-utils/run_sim.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 NAME_OPTS=("adpcm_decode" "adpcm_encode" "aes" "basicmath" "blowfish" "crc" "dijkstra" "fft" "limits" "picojpeg" "qsort" "randmath" "rc4" "rsa" "sha")
-BENCHMARK_NAME="randmath"
+BENCHMARK_NAME="adpcm_decode"
 
-elfmanip.py -v -s 0xc0000000 -i 0x40000000 -o ~/diss/mibench2/$BENCHMARK_NAME/$BENCHMARK_NAME.hex ~/diss/mibench2/$BENCHMARK_NAME/$BENCHMARK_NAME.elf to-hex
+elfmanip.py -v -s 0xc0000000 -i 0x40000000 -o ~/Dissertation-riga/mibench2/$BENCHMARK_NAME/$BENCHMARK_NAME.hex ~/Dissertation-riga/mibench2/$BENCHMARK_NAME/$BENCHMARK_NAME.elf to-hex
 
 
-export CHERI_BGAS_DDRB_HEX_INIT=/home/tim/Documents/Uni/Dissertation/mibench2/$BENCHMARK_NAME/$BENCHMARK_NAME.hex
+export CHERI_BGAS_DDRB_HEX_INIT=~/Dissertation-riga/mibench2/$BENCHMARK_NAME/$BENCHMARK_NAME.hex
 export CHERI_BGAS_PC_RESET_VALUE=0xc0000000
-./cheri-bgas-sim.py  -v --simulation-run-directory ./simulations/$BENCHMARK_NAME
+timeout --foreground 10m ./cheri-bgas-sim.py  -v --simulation-run-directory ./simulations/$BENCHMARK_NAME
